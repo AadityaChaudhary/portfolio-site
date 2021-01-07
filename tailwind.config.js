d16990e3f815cdd6f4d64ec8ea75ed934bec0975
Aaditya <bluebird1339@gmail.com>
2021-01-07 17:04:21 -0500

    fixing config files
    i forgot to check the build locally, and some css attributes didnt build properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,21 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+		  content:  ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+			
+		  options: {
+				safelist: [
+							"from-yellow-400",
+							"to-green-500",
+							"from-blue-400",
+							"to-indigo-600",
+							"from-indigo-400",
+							"to-pink-500",
+							"from-pink-400",
+							"to-red-500",	
+						  ],
+		  }
+  },
+
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
